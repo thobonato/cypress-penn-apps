9164f07c66b14f07c65ccfeafad1d652759cdda2
Thomaz Bonato <thobonato@icloud.com>
2024-09-21 02:53:47 -0400

    working face recog

--- CV/face_recog/test.py
+++ CV/face_recog/test.py
@@ -1,11 +1,10 @@
-import face_recognition
+from deepface import DeepFace
 import cv2 
 import time
 from PIL import Image
 import numpy as np
 
-josh = face_recognition.load_image_file("./faces/josh.JPG")
-josh_enc = face_recognition.face_encodings(josh)[0]
+josh_enc = DeepFace.represent(img_path = "./faces/josh.JPG")
 
 cap = cv2.VideoCapture(0)  # Use 0 for default camera
 
@@ -13,10 +12,11 @@ if not cap.isOpened():
     print("Error: Could not open camera.")
 
 last_inference_time = time.time()
-capture_interval = 1.0  # 2 seconds
+inference_interval = 2.0  # 2 seconds
 current_prediction = "None yet"
 
 while True:
+    time.sleep(inference_interval)
     ret, frame = cap.read()
     if not ret:
         print("Error: Failed to capture frame.")
@@ -25,11 +25,18 @@ while True:
     # Convert the BGR image to RGB
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)   
 
-    # img = Image.fromarray(np.uint8(frame)).convert('RGB')
+    img = Image.fromarray(np.uint8(frame)).convert('RGB')
+    img.save("./faces/temp.png")
 
-    frame_enc = face_recognition.face_encodings(frame)
-    print(f"Frame_enc: {frame_enc}")
-    results = face_recognition.compare_faces([josh_enc], frame_enc)
+    # frame_enc = DeepFace.(frame)
+    # print(f"Frame_enc: {frame_enc}")
+    result = DeepFace.verify(
+        img1_path = "./faces/josh.JPG",
+        img2_path = "./faces/temp.png",
+        enforce_detection = False
+        )
+
+    print(f"Result: {result}")
     
     # cv2.putText(frame, current_prediction, (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 0), 2)
     # cv2.imshow('Gesture Detection', frame)
